625994f3fe025d996d8b834b8d78a1f90376334c
swaglazio <mattis.fautrad@brassart.fr>
2025-11-21 09:55:00 +0100

    ADD(RegionTemplates)

---
+++ Useful Scripts/Utilities/RegionTemplates.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionTemplates : MonoBehaviour
+{
+    #region Fields
+
+    #endregion Fields
+
+    #region Properties
+
+    #endregion Properties
+
+    #region Callbacks
+
+    private void Start()
+    {
+        
+    }
+
+    #endregion Callbacks
+
+    #region Methods
+
+    #endregion Methods
+
+
+}
